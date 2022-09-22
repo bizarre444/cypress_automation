c74618d727bfd5c5f069a44084044c2bcff36759
Bizarre <la_insolence@mail.ru>
2022-09-22 09:29:47 +0300

    Chess reg tests

--- cypress/e2e/registration.spec.js
+++ cypress/e2e/registration.spec.js
@@ -1,3 +1,4 @@
+import { chessRegTo } from "../support/page_objects/chessRegistration"
 import { headerRegistrationTo } from "../support/page_objects/headerRegistration"
 
 describe('Registration tests', () => {
@@ -9,6 +10,13 @@ describe('Registration tests', () => {
     it('Registration flow from header', () => {
 
         headerRegistrationTo.registrationFlow()
+        headerRegistrationTo.checkCorrectUrl()
+    })
+
+    it('Registration flow from chess page', () => {
+
+        chessRegTo.navigateToChessPage()
+        chessRegTo.chessRegFlow()
         headerRegistrationTo.checkCorrectUrl()
     })
 })
--- cypress/support/page_objects/chessRegistration.js
+++ cypress/support/page_objects/chessRegistration.js
@@ -1,5 +1,56 @@
 export class ChessRegistration {
 
+    navigateToChessPage() {
+        cy.visit('https://www.ka-stage.ml/chess-course/')
+    }
+
+    chessRegFlow() {
+        cy.get('.chess-landing-main__content-left-btn')
+            .find('a')
+            .click()
+            //choose Parent flow
+        cy.get('.sign-up-chess__buttons')
+            .find('button')
+            .contains('Parent')
+            .click()
+            //type child's name
+        cy.get('.sign-up-chess__name')
+            .find('input')
+            .type('Kira')
+            //choose the avatar
+        cy.get('.sign-up-chess__avatar-item')
+            .eq(3)
+            .click()
+            //choose the grade
+        cy.get('.sign-up-chess__grade-item')
+            .eq(1)
+            .click()
+            //click Next button
+        cy.get('.sign-up-chess__buttons')
+            .find('button')
+            .contains('Next')
+            .click()
+            //choose the correct answer
+        cy.get('.sign-up-chess__level-question')
+            .eq(0)
+            .find('[for="good-level"]')
+            .click()
+
+        cy.get('.sign-up-chess__buttons')
+            .find('[data-selector="chess-question-1-step-next-btn"]')
+            .click()
+
+        cy.get('.sign-up-chess__cards')
+            .eq(0)
+            .find('[for="know-knight"]')
+            .click()
+
+        cy.get('[data-selector="chess-question-2-step-next-btn"]')
+            .click()
+
+        cy.get('[data-selector="chess-last-step-go-to-plans-button"]')
+            .click()
+    }
 }
 
 export const chessRegTo = new ChessRegistration()
